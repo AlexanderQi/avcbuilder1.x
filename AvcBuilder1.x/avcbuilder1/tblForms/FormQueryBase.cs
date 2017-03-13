bf4e6e1156be42e260593b5b8270a1f34f9abb0b
Alexander Qi Li <VIP_liqi@hotmail.com>
2017-03-14 00:05:12 +0800

    增加遗漏的FormQueryBase.cs

---
+++ AvcBuilder1.x/avcbuilder1/tblForms/FormQueryBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using log4net;
+using mysqlDao_v1;
+using System.Configuration;
+using System.Collections;
+
+
+namespace avcbuilder1.tblForms
+{
+    public partial class FormQueryBase : avcbuilder1.tblForms.FormBase
+    {
+        protected string avc_conn = "";
+        protected mysqlDAO MysqlDao;
+        protected myConnInfo conninfo;
+
+       /// <summary>
+       /// 暴露类单例属性，由子类实现。
+       /// </summary>
+        public static FormQueryBase Instance
+        {
+            get {return null;}
+        }
+
+        protected FormQueryBase()
+        {
+            InitializeComponent();
+            gridView1.ShowFindPanel();
+        }
+
+        virtual public void QueryById(String id) { }
+
+        //DataTable mdt;
+        virtual public void QueryBySql(string sql)
+        {
+//           
+        }
+
+    }
+}

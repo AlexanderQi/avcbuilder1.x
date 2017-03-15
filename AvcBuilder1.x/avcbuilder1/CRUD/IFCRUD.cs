25c777cef5831e0c30a15ef378b03b480d227fd5
AlexanderQi <26205165@qq.com>
2017-03-15 21:03:51 +0800

    Add CRUD interface and  xxx_op entity

---
+++ AvcBuilder1.x/avcbuilder1/CRUD/IFCRUD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace avcbuilder1.CRUD
+{
+    interface IFCRUD
+    {
+        int Add();
+        int Remove();
+        int Update();
+        DataTable Query();           
+    }
+}
---
+++ AvcBuilder1.x/avcbuilder1/CRUD/tblelementstate_op.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvcDb.entities;
+using mysqlDao_v1;
+namespace avcbuilder1.CRUD
+{
+    class tblelementstate_op : tblelementstate, IFCRUD
+    {
+        mysqlDAO MysqlDao;
+        public tblelementstate_op()
+        {
+            MysqlDao = FormConnectSrv.Instance.MySqlDao;
+
+        }
+
+        public int Add()
+        {
+            
+            throw new NotImplementedException();
+        }
+
+        public DataTable Query()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
